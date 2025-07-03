0e09e692ecb7315bf820fd74f49710d1c4173238
Daniel7407 <dpulidoalaniz920@gmail.com>
2025-07-03 21:05:03 +0000

    Create Card class

---
+++ Card.java
@@ -0,0 +1,74 @@
+public class Card {
+    private String suit;
+    private String name;
+    private int value;
+    private String picture;
+
+    // Default constructor
+    public Card() {
+        this.suit = "";
+        this.name = "";
+        this.value = 0;
+        this.picture = "";
+    }
+
+    // Parameterized constructor
+    public Card(String suit, String name, int value, String picture) {
+        this.suit = suit;
+        this.name = name;
+        this.value = value;
+        this.picture = picture;
+    }
+
+    // Accessors (getters)
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    // Mutators (setters)
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return name + " of " + suit + " (Value: " + value + ", Picture: " + picture + ")";
+    }
+
+    // Overriding equals method to compare two Card objects
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Card card = (Card) obj;
+        return value == card.value &&
+               suit.equals(card.suit) &&
+               name.equals(card.name) &&
+               picture.equals(card.picture);
+    }
+}
